812a14ff918657d591355a22611e2bd80b96e264
tiyo5151 <s1F102400224@iniad.org>
2024-06-06 19:30:06 +0900

    nanisunno

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [board, setboard] = [
+  const [board, setboard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,23 +15,34 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+  ]);
+
+  const mino = [0, 1, 2, 3, 4, 5, 6];
 
   return (
     <div className={styles.container}>
       <div className={styles.board}>
-        <div className={styles.gameboard} />
+        <div className={styles.gameboard}>
+          {board.map((row: number[], y: number) => (
+            <div key={y} className={styles.row}>
+              {row.map((cell: number, x: number) => (
+                <div key={x} className={styles.cell}>
+                  <div className={board[y][x] === 1 ? styles.redblock : undefined} />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
-
 export default Home;
