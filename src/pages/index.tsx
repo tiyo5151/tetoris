1d0d85dc33030604d97b284557aef2e9629d48b1
tiyo5151 <s1F102400224@iniad.org>
2024-06-06 20:23:12 +0900

    一個一回下げた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.module.css';
 const Home = () => {
   const [board, setboard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -24,11 +24,23 @@ const Home = () => {
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
   ]);
-
   const mino = [0, 1, 2, 3, 4, 5, 6];
 
+  const predownmino = (event: React.KeyboardEvent) => {
+    if (event.key === 'ArrowDown') {
+      downmino(1, 1);
+    }
+  };
+
+  const downmino = (x: number, y: number) => {
+    const newboard = structuredClone(board);
+    newboard[y][x] = mino[0];
+    newboard[y + 1][x] = mino[1];
+    setboard(newboard);
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} tabIndex={0} onKeyDown={predownmino}>
       <div className={styles.board}>
         <div className={styles.gameboard}>
           {board.map((row: number[], y: number) => (
